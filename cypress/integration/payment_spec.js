6aba825c72706d79db4362f833a33cd15e842b82
Lawrence <lawrence.narh-lawson@boomin.com>
2021-09-03 23:52:15 +0100

    Added pseudo code for integration tests

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,14 @@
+describe("payment", () => {
+  it("user can make payment", () => {
+    //login user
+    //check account balance
+    //click on pay buttonText
+    //search for a user account
+    //add amount and note and click pay
+    //return to transactions
+    //go to personal payments
+    //click on payment
+    //verify if payment was made
+    //verify if payment amount was deducted
+  });
+});
