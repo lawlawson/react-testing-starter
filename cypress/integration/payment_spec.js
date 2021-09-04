597ece9030846125ec63cd6361a59b3124ca6a84
Lawrence <lawrence.narh-lawson@boomin.com>
2021-09-04 17:01:38 +0100

    Wrote all tests to login to app

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -4,6 +4,8 @@ describe("payment", () => {
     cy.visit("/");
     cy.findByRole("textbox", { name: /username/i }).type("johndoe");
     cy.findByLabelText(/password/i).type("s3cret");
+    cy.findByRole("checkbox", { name: /remember me/i }).check();
+    cy.findByRole("button", { name: /sign in/i }).click();
     //check account balance
     //click on pay buttonText
     //search for a user account
